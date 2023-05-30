734aa9dfca368444133362f0031bacc3d4360524
Klayton Junior <klaytonjunior@hotmail.com>
2023-05-30 10:42:10 -0300

    Iniciando projeto

---
+++ src/main/java/com/audsat/msinsurance/MsInsuranceApplication.java
@@ -0,0 +1,13 @@
+package com.audsat.msinsurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsInsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsInsuranceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/audsat/msinsurance/MsInsuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audsat.msinsurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsInsuranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
