82b026ff42b8cd8adce76fbf9184d7c6707c9eaa
Klayton Junior <klaytonjunior@hotmail.com>
2023-05-31 19:46:37 -0300

    Feat: Criando dados de motoristas com seus carros e sinistros

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -28,7 +28,7 @@ values (80000.00, '2022', 'ONIX', 'CHEVROLET');
 
 insert into CARS (FIPE_VALUE, CAR_YEAR, MANUFACTURER, MODEL)
 values (60000.00, '2020', 'ONIX', 'CHEVROLET');
--------------------------------------------------------------
+-------------------------------------------------------------10
 
 --CREATING DRIVERS and CUSTOMERS
 
@@ -79,5 +79,79 @@ values (DATE '1995-01-23', 'CNH-19950123');
 
 insert into CUSTOMER (DRIVER_ID, NAME)
 values ((select id from DRIVERS where DOCUMENT = 'CNH-19950123'), 'Scar');
+
+-------------------------------------------------------------16
+
+--CREATING CAR_DRIVERS
+
+--FOX 2015, Klayton -motorista principal, Nala -outro motorista
+insert into CAR_DRIVERS (IS_MAIN_DRIVER, CAR_ID, DRIVER_ID)
+values (true,
+        (select id from CARS where FIPE_VALUE = 40000.00 AND MANUFACTURER LIKE 'VOLKSWAGEN' AND CAR_YEAR LIKE '2015' AND MODEL LIKE 'FOX'),
+        (select id from DRIVERS where DOCUMENT = 'CNH-123456789')
+ );
+insert into CAR_DRIVERS (IS_MAIN_DRIVER, CAR_ID, DRIVER_ID)
+values (false,
+        (select id from CARS where FIPE_VALUE = 40000.00 AND MANUFACTURER LIKE 'VOLKSWAGEN' AND CAR_YEAR LIKE '2015' AND MODEL LIKE 'FOX'),
+        (select id from DRIVERS where DOCUMENT = 'CNH-20030123')
+);
+
+--UP 2015, Jubileu -motorista principal, Belinha -outro motorista
+insert into CAR_DRIVERS (IS_MAIN_DRIVER, CAR_ID, DRIVER_ID)
+values (true,
+        (select id from CARS where FIPE_VALUE = 35000.00 AND MANUFACTURER LIKE 'UP' AND CAR_YEAR LIKE '2015' AND MODEL LIKE 'FOX'),
+        (select id from DRIVERS where DOCUMENT = 'CNH-123456')
+);
+insert into CAR_DRIVERS (IS_MAIN_DRIVER, CAR_ID, DRIVER_ID)
+values (true,
+        (select id from CARS where FIPE_VALUE = 35000.00 AND MANUFACTURER LIKE 'UP' AND CAR_YEAR LIKE '2015' AND MODEL LIKE 'FOX'),
+        (select id from DRIVERS where DOCUMENT = 'CNH-20050123')
+);
+
+--ONIX 2022, Mufasa -motorista principal, Scar -outro motorista
+insert into CAR_DRIVERS (IS_MAIN_DRIVER, CAR_ID, DRIVER_ID)
+values (true,
+        (select id from CARS where FIPE_VALUE = 80000.00 AND MANUFACTURER LIKE 'ONIX' AND CAR_YEAR LIKE '2022' AND MODEL LIKE 'CHEVROLET'),
+        (select id from DRIVERS where DOCUMENT = 'CNH-19800123')
+);
+insert into CAR_DRIVERS (IS_MAIN_DRIVER, CAR_ID, DRIVER_ID)
+values (true,
+        (select id from CARS where FIPE_VALUE = 80000.00 AND MANUFACTURER LIKE 'ONIX' AND CAR_YEAR LIKE '2022' AND MODEL LIKE 'CHEVROLET'),
+        (select id from DRIVERS where DOCUMENT = 'CNH-19950123')
+);
+
+
+--CELTA 2009, Simba --motorista principal
+insert into CAR_DRIVERS (IS_MAIN_DRIVER, CAR_ID, DRIVER_ID)
+values (true,
+        (select id from CARS where FIPE_VALUE = 12000.00 AND MANUFACTURER LIKE 'CELTA' AND CAR_YEAR LIKE '2009' AND MODEL LIKE 'CHEVROLET'),
+        (select id from DRIVERS where DOCUMENT = 'CNH-19900123')
+);
+
+
+
+--CIVIC 2018, Kiara --motorista principal
+insert into CAR_DRIVERS (IS_MAIN_DRIVER, CAR_ID, DRIVER_ID)
+values (true,
+        (select id from CARS where FIPE_VALUE = 120000.00 AND MANUFACTURER LIKE 'CIVIC' AND CAR_YEAR LIKE '2018' AND MODEL LIKE 'HONDA'),
+        (select id from DRIVERS where DOCUMENT = 'CNH-19990123')
+);
+
+-------------------------------------------------------------
+
+--Criando Claims
+
+--CELTA 2009 - Sinistro
+insert into CLAIMS (EVENT_DATE, CAR_ID)
+values (DATE '2020-01-23',
+        (select id from CARS where FIPE_VALUE = 12000.00 AND MANUFACTURER LIKE 'CELTA' AND CAR_YEAR LIKE '2009' AND MODEL LIKE 'CHEVROLET')
+);
+
+--CIVIC 2018 e Kiara (motorista) - Sinistro
+insert into CLAIMS (EVENT_DATE, CAR_ID, DRIVER_ID)
+values (DATE '2020-01-23',
+        (select id from CARS where FIPE_VALUE = 120000.00 AND MANUFACTURER LIKE 'CIVIC' AND CAR_YEAR LIKE '2018' AND MODEL LIKE 'HONDA'),
+        (select id from DRIVERS where DOCUMENT = 'CNH-19990123')
+);
 
 -------------------------------------------------------------
